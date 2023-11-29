e139332fd2c77252ec64aef6f324526bdb07420a
Cartman223 <jpirmao37@gmail.com>
2023-11-29 15:42:36 -0300

    add best of five functionality

--- javascript.js
+++ javascript.js
@@ -1,34 +1,94 @@
+let playerScore = 0;
+let computerScore = 0;
+
 function getComputerChoice() {
     const array = ["Rock", "Paper", "Scissors"];
     let getIndex = array[Math.floor(Math.random() * (3 - 0) + 0)];
+    
     return getIndex;
 }
 
 function round(playerSelection, computerSelection) {
     if (playerSelection === "Rock" && computerSelection === "Paper") {
-        return "You lose! Paper beats Rock!";
+        console.log("Player's choice was: " + playerSelection);
+        console.log("Computer's choice was: " + computerSelection);
+        console.log("You lose! Paper beats Rock!")
+        
+        return computerScore++;
     } else if (playerSelection === "Rock" && computerSelection === "Scissors") {
-        return "You win! Rock beats Scissors!";
+        console.log("Player's choice was: " + playerSelection);
+        console.log("Computer's choice was: " + computerSelection);
+        console.log("You win! Rock beats Scissors!")
+        
+        return playerScore++;
     } else if (playerSelection === "Rock" && computerSelection === "Rock") {
-        return round(playerSelection = prompt("Tie Break!"), computerSelection);
+        let playerInput = prompt("Tie Break!");
+        let playerSelection = fixInput(playerInput);
+        let computerSelection = getComputerChoice();
+        
+        return round(playerSelection, computerSelection);
     } else if (playerSelection === "Paper" && computerSelection === "Rock") {
-        return "You win! Paper beats Rock!";
+        console.log("Player's choice was: " + playerSelection);
+        console.log("Computer's choice was: " + computerSelection);
+        console.log("You win! Paper beats Rock!")
+        
+        return playerScore++;
     } else if (playerSelection === "Paper" && computerSelection === "Scissors") {
-        return "You lose! Scissors beats Paper!";
+        console.log("Player's choice was: " + playerSelection);
+        console.log("Computer's choice was: " + computerSelection);
+        console.log("You lose! Scissors beats Paper!");
+        
+        return computerScore++;
     } else if (playerSelection === "Paper" && computerSelection === "Paper") {
-        return round(playerSelection = prompt("Tie Break!"), computerSelection);
+        let playerInput = prompt("Tie Break!");
+        let playerSelection = fixInput(playerInput);
+        let computerSelection = getComputerChoice();
+        
+        return round(playerSelection, computerSelection);
     } else if (playerSelection === "Scissors" && computerSelection === "Rock") {
-        return "You lose! Rock beats Scissors!";
+        console.log("Player's choice was: " + playerSelection);
+        console.log("Computer's choice was: " + computerSelection);
+        console.log("You lose! Rock beats Scissors!");
+        
+        return computerScore++;
     } else if (playerSelection === "Scissors" && computerSelection === "Paper") {
-        return "You win! Scissors beats Paper!";
+        console.log("Player's choice was: " + playerSelection);
+        console.log("Computer's choice was: " + computerSelection);
+        console.log("You win! Scissors beats Paper!")
+        
+        return playerScore++;
     } else if (playerSelection === "Scissors" && computerSelection === "Scissors") {
-        return round(playerSelection = prompt("Tie Break!"), computerSelection);
+        let playerInput = prompt("Tie Break!");
+        let playerSelection = fixInput(playerInput);
+        let computerSelection = getComputerChoice();
+        
+        return round(playerSelection, computerSelection);
     }
 }
 
-const playerSelection = prompt("Rock, Paper or Scissors?");
-const computerSelection = getComputerChoice();
+function fixInput(input) {
+    if (input) {
+      return input = input.slice(0,1).toUpperCase() + input.slice(1).toLowerCase();
+    } else {
+        return input = "Invalid Choice!";
+    }
+}
 
-console.log("Player's choice was: " + playerSelection);
-console.log("Computer's choice was: " + computerSelection);
-console.log(round(playerSelection, computerSelection));
+function game() {
+    for (let i = 0; i < 5; i++) {
+        let playerInput = prompt("Rock, Paper or Scissors?");
+        let playerSelection = fixInput(playerInput);
+        let computerSelection = getComputerChoice();
+        round(playerSelection, computerSelection);
+    }
+    console.log(`Your score is ${playerScore}`);
+    console.log(`The Computer's score is ${computerScore}`);
+    playerScore > computerScore ? console.log("You Win!") : console.log("Computer Wins!");
+}
+
+game();
+
+// play a best-of-five game
+// keep score
+// report a winner or loser at the end
+
